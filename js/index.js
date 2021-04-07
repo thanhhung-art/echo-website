265c1d361209112ecce2c1db15bc5a415bf69293
thanh-hung <65265858+thanhhung-art@users.noreply.github.com>
2021-04-07 19:41:37 +0700

    responsive 50%

--- js/index.js
+++ js/index.js
@@ -2,28 +2,24 @@
 
 $(document).ready( 
 	$('.wrap-slider').slick({
-  		dots: false,
+  		dots: true,
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
-
-          pauseOnHover: true,
+		pauseOnHover: true,
 
         responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-            }
-        },
         {
             breakpoint: 768,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                prevArrow: false,
+    			nextArrow: false,
+    			dots: false,
+    			pauseOnHover: false,
             }
         }
   ]
