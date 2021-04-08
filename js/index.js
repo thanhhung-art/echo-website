0241e39088410205aec85a488ac062707ce692c5
thanh-hung <65265858+thanhhung-art@users.noreply.github.com>
2021-04-08 14:44:50 +0700

    responsive 90%

--- js/index.js
+++ js/index.js
@@ -8,21 +8,35 @@ $(document).ready(
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
-		pauseOnHover: true,
-
-        responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                prevArrow: false,
-    			nextArrow: false,
-    			dots: false,
-    			pauseOnHover: false,
-            }
-        }
-  ]
-
+    	responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 1,
+		        infinite: true,
+		        dots: true
+		      }
+		    },
+		    {
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+		        arrows: false,
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+		        arrows: false,
+		      }
+		    }
+		    // You can unslick at a given breakpoint now by adding:
+		    // settings: "unslick"
+		    // instead of a settings object
+  		]
  })	
 );
