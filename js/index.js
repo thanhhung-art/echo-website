7d707ea87e1f799c1085932fc39261ec4432637e
thanh-hung <65265858+thanhhung-art@users.noreply.github.com>
2021-04-09 10:04:08 +0700

    add effect on submenu mobile

--- js/index.js
+++ js/index.js
@@ -48,6 +48,7 @@ let closeMenu = document.getElementById("button-close-menu");
 let navbarMobile = document.getElementById("navbar-mobile");
 let submenu = document.getElementById("submenu");
 let submenu2 = document.getElementById("submenu-2");
+let about = document.getElementById("about");
 
 function handleOpen (){
 	navbarMobile.style.right = "0";
@@ -58,19 +59,26 @@ function handleClose () {
 }
 
 function handleSubmenu () {
-	if(submenu.nextElementSibling.style.display === "none"){
-		submenu.nextElementSibling.style.display = "block";
+	if(submenu.nextElementSibling.style.height === "0px" ){
+		submenu.nextElementSibling.style.height = "80px";
+		submenu.nextElementSibling.style.opacity = "1";
 	}
 	else{
-		submenu.nextElementSibling.style.display = "none";
+		submenu.nextElementSibling.style.height = "0px";
+		submenu.nextElementSibling.style.opacity = "0";
 	}
 }
 
 function handleSubmenu2 () {
-	if(submenu2.nextElementSibling.style.display === "none"){
-		submenu2.nextElementSibling.style.display = "block";
+	if(submenu2.nextElementSibling.style.height === "0px" ){
+		submenu2.nextElementSibling.style.height = "80px";
+		submenu2.nextElementSibling.style.opacity = "1";
+		about.style.paddingTop = "4.4rem";
+
 	}
 	else{
-		submenu2.nextElementSibling.style.display = "none";
+		submenu2.nextElementSibling.style.height = "0px";
+		submenu2.nextElementSibling.style.opacity = "0";
+		about.style.paddingTop = "0px";
 	}
 }
